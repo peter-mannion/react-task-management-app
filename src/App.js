faee029613cec6f72fd54ef34acc1b617d590e46
P M <pvmannion@gmail.com>
2026-05-11 21:33:21 -0700

    Create navbar and add styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, NavLink } from "react-router";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        {/* Navigation Bar */}
+        <nav className="nav-bar-style">
+          <div className="logo">Task Management App</div>
+          <ul className="ul-style">
+            <li>
+              <NavLink to="/" className="nav-link">
+                Tasks
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/contact" className="nav-link">
+                Contact
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </Router>
   );
 }
 
