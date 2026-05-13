93d51b77d41b10822d2e68d8044ffb15f833ab63
P M <pvmannion@gmail.com>
2026-05-12 20:16:53 -0700

    Retain 'tasks' after browser refresh with useEffect

--- src/components/TMContainer/TMContainer.jsx
+++ src/components/TMContainer/TMContainer.jsx
@@ -1,11 +1,20 @@
 // Parent Container
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import TaskInput from "../TaskInput/TaskInput";
 import TaskItem from "../TaskCard/TaskCard";
 
 const TMContainer = () => {
-  const [tasks, setTasks] = useState([]);
+  // Retain "tasks" even if user refreshes the browswer or navigates between TMContainer and ContactForm - initialize state using browser's local storage.
+  const [tasks, setTasks] = useState(() => {
+    const savedTasks = localStorage.getItem("myTasks");
+    return savedTasks ? JSON.parse(savedTasks) : [];
+  });
+
+  // Save to browser's local storage whenever "tasks" array is updated
+  useEffect(() => {
+    localStorage.setItem("myTasks", JSON.stringify(tasks));
+  }, [tasks]);
 
   const addTask = (text) => {
     const newTask = {
